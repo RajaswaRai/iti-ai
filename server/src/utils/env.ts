f469c19588b340f0dabc2eb15143af6556427f5d
JavanicusErecus <garxboss@gmail.com>
2026-03-14 13:16:10 +0700

    feat: Final fix 2.1: generateContent => generateContentStream, fix Prompt

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -8,7 +8,7 @@
     export const GEMINI_EMBEDDING_MODEL: string = process.env.GEMINI_EMBEDDING_MODEL|| "gemini-embedding-001";
     export const GEMINI_TEMPERATURE: number = Number(process.env.GEMINI_TEMPERATURE) || 0.5;
     export const GEMINI_TOPP: number = Number(process.env.GEMINI_TOPP) || 0.9;
-    export const GEMINI_MAX_RES: number = Number(process.env.GEMINI_MAX_RES) || 300;
+    // export const GEMINI_MAX_RES: number = Number(process.env.GEMINI_MAX_RES) || 300;
     export const GEMINI_INSTRUCT: string = process.env.GEMINI_INSTRUCT || "";
 
     export const PINECONE_API_KEY = process.env.PINECONE_API_KEY || '';
